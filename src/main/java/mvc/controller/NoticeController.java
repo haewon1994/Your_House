16330d935a14bdbb500be2823acc3d405f04a837
DOYOON\meila <ehdbs71@nver.com>
2022-10-27 17:24:08 +0900

    도윤 노티스 스토리 컨트롤러 스토리다오임플 노티스라이트

--- src/main/java/mvc/controller/NoticeController.java
+++ src/main/java/mvc/controller/NoticeController.java
@@ -87,7 +87,7 @@ public class NoticeController implements Controller {
 		
 		notiService.insert(notice);
 
-	   return new ModelAndView("front", true);//key=elec&methodName=select 기본으로 설정된다.	
+	   return new ModelAndView("admin", true);//key=elec&methodName=select 기본으로 설정된다.	
 	}
 	
 	/**
@@ -120,7 +120,7 @@ public class NoticeController implements Controller {
 
 		request.setAttribute("notice", notice);
 		
-		return new ModelAndView("elec/update.jsp");//forward방식
+		return new ModelAndView("notice/update.jsp");//forward방식
 	}
 	
 	/**
@@ -138,7 +138,7 @@ public class NoticeController implements Controller {
 		
 		//수정이 완료가 된후....
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("front?key=elec&methodName=selectBynoticeCode&noticeCode="+noticeCode);
+		mv.setViewName("admin?key=elec&methodName=selectBynoticeCode&noticeCode="+noticeCode);
 	    mv.setRedirect(true);
 		return mv;
 	}
@@ -157,7 +157,7 @@ public class NoticeController implements Controller {
 		
 		String saveDir = request.getServletContext().getRealPath("/save");
 		notiService.delete(Integer.parseInt(noticeCode), saveDir);
-		 return new ModelAndView("front",true);
+		 return new ModelAndView("admin",true);
 	
 	}
 	
